a43a35b06a9a20ca32bad8a3d1a9b6bdaf953e0f
ckyeon <qsky8520@gmail.com>
2023-03-30 02:44:38 +0900

    initial commit

---
+++ src/main/java/com/sequence/anonymous/AnonymousApplication.java
@@ -0,0 +1,16 @@
+package com.sequence.anonymous;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/")})
+public class AnonymousApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnonymousApplication.class, args);
+  }
+
+}
