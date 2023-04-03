32c66c1a900b1293c73479a1bf3d0cbe5227d40d
ckyeon <qsky8520@gmail.com>
2023-04-03 21:28:51 +0900

    refactor(OAuth2): move getProvider() method to OAuth2Provider enum

--- src/main/java/com/sequence/anonymous/security/CustomOAuth2UserService.java
+++ src/main/java/com/sequence/anonymous/security/CustomOAuth2UserService.java
@@ -20,14 +20,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
     OAuth2User oAuth2User = super.loadUser(userRequest);
-    OAuth2Provider oAuth2Provider = getProvider(userRequest);
+    OAuth2Provider oAuth2Provider = OAuth2Provider.getProvider(userRequest);
     User user = processUser(oAuth2Provider, oAuth2User.getAttributes());
     return new CustomOAuth2User(oAuth2User, user.getId(), user.getRole());
-  }
-
-  private static OAuth2Provider getProvider(OAuth2UserRequest userRequest) {
-    String registrationId = userRequest.getClientRegistration().getRegistrationId();
-    return OAuth2Provider.valueOf(registrationId.toUpperCase());
   }
 
   private User processUser(OAuth2Provider provider, Map<String, Object> attributes) {
--- src/main/java/com/sequence/anonymous/user/domain/OAuth2Provider.java
+++ src/main/java/com/sequence/anonymous/user/domain/OAuth2Provider.java
@@ -1,6 +1,13 @@
 package com.sequence.anonymous.user.domain;
 
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+
 public enum OAuth2Provider {
   GOOGLE,
-  GITHUB
+  GITHUB;
+
+  public static OAuth2Provider getProvider(OAuth2UserRequest userRequest) {
+    String registrationId = userRequest.getClientRegistration().getRegistrationId();
+    return OAuth2Provider.valueOf(registrationId.toUpperCase());
+  }
 }
