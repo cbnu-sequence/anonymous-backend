26a257c58acbfeae61f969836001e2fd227f0961
mk3058 <kmk3058@icloud.com>
2023-09-04 13:22:46 +0900

    fix(matchPost): MatchServicePost 클래스를 순환 참조하던 문제 수정

--- src/main/java/com/sequence/anonymous/matchpost/application/MatchPostService.java
+++ src/main/java/com/sequence/anonymous/matchpost/application/MatchPostService.java
@@ -1,10 +1,11 @@
 package com.sequence.anonymous.matchpost.application;
 
+import com.sequence.anonymous.matchpost.domain.repository.MatchPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MatchPostService {
-  private final MatchPostService matchPostService;
+  private final MatchPostRepository matchPostRepository;
 }
