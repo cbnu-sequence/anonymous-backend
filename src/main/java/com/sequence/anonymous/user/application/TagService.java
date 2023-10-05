89c067072c338092373c9a95e3eaabbc4fffd280
hanachooi <jjina.js@gmail.com>
2023-10-05 21:16:23 +0900

    성격 태그 가져오기

--- src/main/java/com/sequence/anonymous/user/application/TagService.java
+++ src/main/java/com/sequence/anonymous/user/application/TagService.java
@@ -1,6 +1,7 @@
 package com.sequence.anonymous.user.application;
 
 import com.sequence.anonymous.user.domain.repository.TagRepository;
+import com.sequence.anonymous.user.domain.tag.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,9 @@ import org.springframework.stereotype.Service;
 public class TagService {
 
   private final TagRepository tagRepository;
+
+
+  public Tag findById(Long id){
+    return tagRepository.findById(id).orElseThrow(()->new IllegalArgumentException("not found: "+id));
+  }
 }
--- src/main/java/com/sequence/anonymous/user/presentation/TagController.java
+++ src/main/java/com/sequence/anonymous/user/presentation/TagController.java
@@ -1,14 +1,24 @@
 package com.sequence.anonymous.user.presentation;
 
 import com.sequence.anonymous.user.application.TagService;
+import com.sequence.anonymous.user.domain.dto.TagResponse;
+import com.sequence.anonymous.user.domain.tag.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/tags")
+@RequestMapping("/api/personality-tags")
 public class TagController {
-
   private final TagService tagService;
+
+  public ResponseEntity<TagResponse> findTag(@PathVariable Long id){
+    Tag tag = tagService.findById(id);
+
+
+    return ResponseEntity.ok().body(new TagResponse(tag));
+  }
 }
