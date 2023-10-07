a1104fd33599ef03134d1c32ea6f631149596532
mk3058 <kmk3058@icloud.com>
2023-10-07 13:39:25 +0900

    feat(FriendController): spring security를 통해 user 정보를 얻도록 수정

--- src/main/java/com/sequence/anonymous/friend/presentation/FriendController.java
+++ src/main/java/com/sequence/anonymous/friend/presentation/FriendController.java
@@ -3,17 +3,19 @@ package com.sequence.anonymous.friend.presentation;
 import com.sequence.anonymous.friend.application.FriendService;
 import com.sequence.anonymous.friend.domain.Friend;
 import com.sequence.anonymous.friend.presentation.dto.FriendResponse;
+import com.sequence.anonymous.security.CustomOAuth2User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.webjars.NotFoundException;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +25,9 @@ public class FriendController {
   private final FriendService friendService;
 
   @GetMapping("")
-  public ResponseEntity<List<FriendResponse>> findFriends() {
-    Long userId = 1L; //spring security를 통해 인증 정보 찾을 예정
+  public ResponseEntity<List<FriendResponse>> findFriends(
+      @AuthenticationPrincipal CustomOAuth2User user) {
+    Long userId = getUserIdFromCustomOAuth2User(user);
 
     List<Friend> friendList = friendService.findFriendByUserId(userId);
     List<FriendResponse> friendResponseList = friendList.stream()
@@ -34,18 +37,20 @@ public class FriendController {
   }
 
   @PostMapping("/{id}/invitation")
-  public ResponseEntity<Void> addFriend(@PathVariable Long id) {
-    Long userId = 1L; //spring security를 통해 인증 정보 찾을 예정
+  public ResponseEntity<Void> addFriend(@PathVariable Long id,
+      @AuthenticationPrincipal CustomOAuth2User user) {
+    Long userId = getUserIdFromCustomOAuth2User(user);
 
     friendService.invite(userId, id);
     return ResponseEntity.status(HttpStatus.OK).build();
   }
 
   @GetMapping("/invites")
-  public ResponseEntity<List<FriendResponse>> findInvites() {
-    Long id = 1L; //spring security를 통해 인증 정보 찾을 예정
+  public ResponseEntity<List<FriendResponse>> findInvites(
+      @AuthenticationPrincipal CustomOAuth2User user) {
+    Long userId = getUserIdFromCustomOAuth2User(user);
 
-    List<Friend> friendList = friendService.findInviteByUserId(id);
+    List<Friend> friendList = friendService.findInviteByUserId(userId);
     List<FriendResponse> friendResponseList = friendList.stream()
         .map(FriendResponse::new)
         .toList();
@@ -68,5 +73,14 @@ public class FriendController {
   public ResponseEntity<Void> deleteFriend(@PathVariable Long id) {
     friendService.deleteFriend(id);
     return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  private static Long getUserIdFromCustomOAuth2User(CustomOAuth2User user) {
+    Object object = user.getAttribute("id");
+
+    if (object == null) {
+      throw new RuntimeException("cannot retrieve user id");
+    }
+    return Long.parseLong(object.toString());
   }
 }
