6a2419dc167fd7d88821d8e6a84443a52b94890d
mk3058 <kmk3058@icloud.com>
2023-10-09 02:18:17 +0900

    refactor(FriendResponse): 친구 목록 조회시 인증 주체의 정보는 반환하지 않도록 수정

--- src/main/java/com/sequence/anonymous/friend/presentation/dto/FriendResponse.java
+++ src/main/java/com/sequence/anonymous/friend/presentation/dto/FriendResponse.java
@@ -4,16 +4,15 @@ import com.google.common.base.Preconditions;
 import com.sequence.anonymous.friend.domain.Friend;
 import com.sequence.anonymous.user.domain.user.User;
 
-public record FriendResponse (Long id, User user, User friend) {
+public record FriendResponse (Long id, User friend) {
 
   public FriendResponse {
-    Preconditions.checkArgument(user != null, "user must be provided");
     Preconditions.checkArgument(friend != null, "friend must be provided");
   }
 
   public static FriendResponse fromFriend(Friend friend) {
     Preconditions.checkArgument(friend != null, "friend must be provided");
 
-    return new FriendResponse(friend.getId(), friend.getUser(), friend.getFriend());
+    return new FriendResponse(friend.getId(), friend.getFriend());
   }
 }
